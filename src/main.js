e95538225f946bca2f1f2b5e6a40ddf54b3c177f
Ilya Kotovskiy <ilya.kot.web@gmail.com>
2024-07-28 15:11:32 +0300

    sprint 1 modificated 1

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import Handlebars from 'handlebars';
 import NotFoundPage from './pages/404';
 import ServerErrorPage from './pages/500';
 import AuthPage from './pages/Аuthorization';
+import SettingsPage from './pages/Settings';
 
 const app = document.getElementById('app');
+const username = 'Илья';
 
 const page_404 = NotFoundPage();
 const page_500 = ServerErrorPage();
@@ -35,7 +37,33 @@ const page_register = AuthPage({
         'Авторизоваться'
     ]
 });
+const page_settings = SettingsPage({
+    name: username,
+    profile_settings: [
+        {title_input: 'Имя:', type_input: 'text', name_input: 'first_name', value_input: 'Илья'},
+        {title_input: 'Фамилия:', type_input: 'text', name_input: 'second_name', value_input: 'Котовский'},
+        {title_input: 'Никнейм:', type_input: 'text', name_input: 'display_name', value_input: 'Iluha'},
+        {title_input: 'Логин:', type_input: 'text', name_input: 'login', value_input: 'ilya.kot'},
+        {title_input: 'Почта:', type_input: 'email', name_input: 'email', value_input: 'ilya.kot.web@gmail.com'},
+        {title_input: 'Номер телефона:', type_input: 'tel', name_input: 'phone', value_input: '+79964181340'},
+    ],
+    buttons_settings: [
+        {theme: 'red', title: 'Выйти'},
+        {theme: 'blue', title: 'Сменить пароль'}
+    ]
+});
+const page_changePassword = SettingsPage({
+    name: username,
+    profile_settings: [
+        {title_input: 'Старый пароль:', type_input: 'password', name_input: 'oldPassword', value_input: 'oldpass'},
+        {title_input: 'Новый пароль:', type_input: 'password', name_input: 'newPassword', value_input: 'newpassword783'},
+    ],
+    buttons_settings: [
+        {theme: 'red', title: 'Назад'},
+        {theme: 'blue', title: 'Сохранить новый пароль'}
+    ]
+})
 
-const result = Handlebars.compile(page_register)();
+const result = Handlebars.compile(page_settings)();
 
 app.innerHTML = result;
