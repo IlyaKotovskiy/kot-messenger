8dd489f20b0028577b5d81cb317c40367df2c3a4
Elias <118120222+IlyaKotovskiy@users.noreply.github.com>
2024-10-03 13:01:51 +0300

    Revert "Sprint 2"

--- src/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-// Шаблоны
-declare module '*.hbs?raw' {
-  const content: string;
-  export default content;
-}
--- src/utils/compiler.ts
+++
@@ -1,7 +0,0 @@
-import Handlebars from 'handlebars';
-
-function compiler(template: string, props: Object): string {
-  return Handlebars.compile(template)(props);
-}
-
-export default compiler;
